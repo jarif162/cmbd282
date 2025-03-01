d40d72f6229b000599a614729dd84c1d39503c31
jarif162 <jarifsudin6@gmail.com>
2025-03-01 14:32:08 +0600

    done

---
+++ regularclass/d16-l1-php-file-system.php
@@ -0,0 +1,31 @@
+<?php
+//basename()
+$path=$_SERVER['PHP_SELF'];
+echo $path."<br>";
+$file=basename($path) ;
+echo $file."<br>";
+
+//dirname()
+$dir=dirname($path);
+echo $dir."<br>";
+
+//copy()
+copy("d6-l1-loops.php","d6-l1-loops-copy.php");
+
+//file()
+$file=file("d6-l1-loops.php");
+echo "<pre>";
+print_r($file);
+echo "</pre>"; 
+
+//file_exist
+if(file_exists("d6-l1-loops.php")){
+    echo "File exist";
+}else{
+    echo "File not exist";
+}
+
+echo "<br>";
+
+
+?>
