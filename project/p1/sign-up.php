49af8cd32de8ef20e178940e1b46aa7e31abe626
Jarif Foysal <jarifsudin6@gmail.com>
2025-06-05 04:15:17 +0600

    h

--- project/p1/sign-up.php
+++ project/p1/sign-up.php
@@ -25,13 +25,15 @@ if (isset($_POST['signup123'])) {
 
     if (empty($password)) {
         $errPassword = "Password is required";
-    } elseif (strlen($password) < 8) {
+    }
+    // elseif (!preg_match("/ ^[A-Za-z0-9!@#$^&+=]{8,}$/", $password)) 
+    elseif (strlen($password) < 6) {
         $errPassword = "Password must be at least 6 characters long";
     } else {
         $crrPassword = password_hash($password, PASSWORD_DEFAULT);
     }
 
-    if (isset($confirmPassword)) {
+    if (empty($confirmPassword)) {
         $errConfirmPassword = "Confirm Password is required";
     } elseif ($confirmPassword !== $password) {
         $errConfirmPassword = "Passwords do not match";
@@ -62,18 +64,41 @@ if (isset($_POST['signup123'])) {
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password">
+                        <input type="password" class="form-control <?= isset($errPassword) ? "is-invalid" : null  ?>" id="password" name="password" value="<?= $password ?? null ?>">
+                        <div class="invalid-feedback"><?= $errPassword ?? null ?></div>
                     </div>
                     <div class="mb-3">
                         <label for="confirmPassword" class="form-label">Confirm Password</label>
-                        <input type="password" class="form-control <?= isset($errPassword) ? "is-invalid" : null ?> " id="confirmPassword" name="confirmPassword">
-                        <div class="invalid-feedback"><?= $errPassword ?? null ?></div>
+                        <input type="password" class="form-control <?= isset($errConfirmPassword) ? "is-invalid" : null ?>" id="confirmPassword" name="confirmPassword" value="<?= $confirmPassword ?? null ?>">
+                        <div class="invalid-feedback"><?= $errConfirmPassword ?? null ?></div>
+                    </div>
+                    <div class="mb-3">
+                        <!-- showpassword -->
+                        <input type="checkbox" id="showPass" class="form-check-input">
+                        <label for="showPass">Show Password</label>
                     </div>
                     <button type="submit" class="btn btn-primary " name="signup123">sign up</button>
                     <p class="mt-3">Already have an account? <a href="sign-in.php">sign in</a></p>
                 </div>
         </div>
     </div>
+
+    <script>
+        const showPass = document.getElementById('showPass');
+        const password = document.getElementById('password');
+        const confirmPassword = document.getElementById('confirmPassword');
+        showPass.addEventListener('clicks', function() {
+            if (this.checked) {
+                password.setAttribute('type', 'text');
+                confirmPassword.setAttribute('type', 'text');
+            } else {
+                password.setAttribute('type', 'password');
+                confirmPassword.setAttribute('type', 'password');
+            }
+        });
+
+        //here this and showPass are used to toggle the password visibility and this use instead of showPass
+    </script>
 
 
 
